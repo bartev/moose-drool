3af4fb6d19dfc456075dc7f28947cee4ab1d9809
Bartev <bartev@gmail.com>
2014-01-22 13:31:41 -0800

    Getting back to MLIA

--- mach-lrn-in-act/ch06-svm/svm-mlia.py
+++ mach-lrn-in-act/ch06-svm/svm-mlia.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+# import pandas as pd
 
 # from numpy import mat, zeros, random
 
@@ -14,6 +14,8 @@ def loadDataSet(fileName):
 	return dataMat, labelMat
 
 def selectJrand(i, m):
+	""" Select any J not equal to i
+	"""
 	j = i
 	while (j == i):
 		j = int(np.random.uniform(0, m))
@@ -64,7 +66,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 					L = max(0, alphas[j] + alphas[i] - C)
 					H = min(C, alphas[j] + alphas[i])
 				if L == H:
-					print "L == H"
+					# print "L == H"
 					continue
 				# eta = optimal amt to change alpha[j]
 				eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - \
@@ -79,7 +81,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 				alphas[j] = clipAlpha(alphas[j], H, L)
 				# Quit for loop if alpha didn't change much
 				if (abs(alphas[j] - alphaJold) < 0.00001):
-					print "j not moving enough"
+					# print "j not moving enough"
 					continue
 				# update i by same amount as j in opposite directin
 				# Change alpha[i] by opposite sign
@@ -89,7 +91,6 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 					dataMatrix[i, :] * dataMatrix[i, :].T - \
 					labelMat[j] * (alphas[j] - alphaJold) * \
 					dataMatrix[i, :] * dataMatrix[j, :].T
-					# i vs j here?
 				b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * \
 					dataMatrix[i, :] * dataMatrix[j, :].T - \
 					labelMat[j] * (alphas[j] - alphaJold) * \
@@ -101,7 +102,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 				else:
 					b = (b1 + b2)/2.0
 				alphaPairsChanged += 1
-				print "iter: %d i: %d, pairs changed %d" % (iter, i, alphaPairsChanged)
+				# print "iter: %d i: %d, pairs changed %d" % (iter, i, alphaPairsChanged)
 		#  Exit while loop if go through maxIter times without changing alphas
 		if (alphaPairsChanged == 0):
 			iter += 1
@@ -109,10 +110,34 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
 			iter = 0
 		print "iteration number: %d" % iter
 	return b, alphas
-	
+
+def kernelTrans(X, A, kTup):
+	""" kTup is a tuple that containes information about the kernel
+		kTup[0]: string describing what type of kernel
+		kTup[1...]: optional argument that may be needed
+	"""
+	m, n = np.shape(X)
+	K = np.mat(np.zeros((m, 1)))
+	if kTup[0] == 'lin':
+		# linear kernel
+		# dot product between dataset and row of dataset
+		K = X * A.T
+	elif kTup[0] == 'rbf':
+		# radial basis function (Gaussian)
+		for j in range(m):
+			deltaRow = X[j, :] - A
+			K[j] = deltaRow * deltaRow.T
+		# element-wise division
+		K = np.exp(K / (-1 * kTup[1] ** 2))
+	# add additional kernel types here
+	# elif ... 
+	else:
+		raise NameError('Houston, we have a problem - that kernel is not recognized')
+	return K
+
 class optStruct:
 	""" data structure to hold all important values """
-	def __init__(self, dataMatIn, classLabels, C, toler):
+	def __init__(self, dataMatIn, classLabels, C, toler, kTup):
 		self.X = dataMatIn
 		self.labelMat = classLabels
 		self.C = C
@@ -132,46 +157,23 @@ class optStruct:
 		for i in range(self.m):
 			self.K[:, i] = kernelTrans(self.X, self.X[i, :], kTup)
 
-def calcEkNoKernel(oS, k):
-	""" calculate an E value for a given alpha. 
-		Before done inline, but in full Platt SMO, 
-		calculation is done more often, so pull it out 
-	"""
-	"""
-		fXi = Prediction of class
-		np.multiply(alphas, labelMat) does element-wise 
-		multiplication resulting in alpha(i) * y(i) terms and 
-		a (1 x m) matrix (after the transform)
-		(dataMatrix * dataMatrix[i,:].T) is the inner product 
-		of all the rows of x with the ith row. 
-		It is a (m x 1) matrix
-		Multiplying these 2, gives the 
-		sum(i=1 to m) [ alpha(i) y(i) <x(i), x>], 
-		the main term in calculating the predicted class.
-	"""
-	fXk = float( np.multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k, :].T) + oS.b)
-	Ek = fXk - float(oS.labelMat[k])
-	return Ek
-
 def calcEk(oS, k):
 	""" calculate an E value for a given alpha. 
 		Before done inline, but in full Platt SMO, 
 		calculation is done more often, so pull it out 
 	"""
 	"""
-		fXi = Prediction of class
+		fXk = Prediction of class
 		np.multiply(alphas, labelMat) does element-wise 
 		multiplication resulting in alpha(i) * y(i) terms and 
 		a (1 x m) matrix (after the transform)
-		(dataMatrix * dataMatrix[i,:].T) is the inner product 
-		of all the rows of x with the ith row. 
+		oS.K[:, k] is the kernel (inner product 
+		of all the rows of x with the ith row?). 
 		It is a (m x 1) matrix
 		Multiplying these 2, gives the 
 		sum(i=1 to m) [ alpha(i) y(i) <x(i), x>], 
 		the main term in calculating the predicted class.
 	"""
-	# Changed this line for kernel version
-	# fXk = float( np.multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k, :].T) + oS.b)
 	fXk = float( np.multiply(oS.alphas, oS.labelMat).T * oS.K[:, k] + oS.b)
 	Ek = fXk - float(oS.labelMat[k])
 	return Ek
@@ -206,12 +208,171 @@ def updateEk(oS, k):
 	Ek = calcEk(oS, k)
 	oS.eCache[k] = [1, Ek]
 
-def innerLnoKernel(i, oS):
+
+def innerL(i, oS):
+	Ei = calcEk(oS, i)
+	if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or \
+		((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+		j, Ej = selectJ(i, oS, Ei)
+		alphaIold = oS.alphas[i].copy()
+		alphaJold = oS.alphas[j].copy()
+		if (oS.labelMat[i] != oS.labelMat[j]):
+			L = max(0, oS.alphas[j] - oS.alphas[i])
+			H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+		else:
+			L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+			H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+		if L == H:
+			print "L == H"
+			return 0
+		# Change to use kernel
+		eta = 2.0 * oS.K[i, j] - oS.K[i, i] - oS.K[j, j]
+		if eta >= 0:
+			print "eta >= 0"
+			return 0
+		oS.alphas[j] -= oS.labelMat[j] * (Ei - Ej) / eta
+		oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+		# update ecache (j)
+		updateEk(oS, j)
+		if (abs(oS.alphas[j] - alphaJold) < 0.00001):
+			# print "j not moving enough"
+			return 0
+		oS.alphas[i] += oS.labelMat[j] * oS.labelMat[i] * (alphaJold - oS.alphas[j])
+		# update ecache (i)
+		updateEk(oS, i)
+		# Change to use kernel
+		b1 = oS.b - Ei - \
+			oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.K[i, i] - \
+			oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[i, j]
+		b2 = oS.b - Ej - \
+			oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.K[i, j] - \
+			oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.K[j, j]
+		if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]):
+			oS.b = b1
+		elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]):
+			oS.b = b2
+		else:
+			oS.b = (b1 + b2)/2.0
+		return 1
+	else:
+		return 0
+
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+	"""  Full Platt SMO - same inputs as simple version
+	"""
+	# create data structure to hold all the data
+	oS = optStruct(np.mat(dataMatIn), np.mat(classLabels).transpose(), C, toler, kTup)
+	# initialize variables
+	iter = 0
+	entireSet = True
+	alphaPairsChanged = 0
+	# main code - similar to smoSimple, but more exit conditions
+	while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+		alphaPairsChanged = 0
+		if entireSet:
+			# go over all values
+			for i in range(oS.m):
+				# call innerL to choose 2nd alpha and do optimization if possible
+				alphaPairsChanged += innerL(i, oS)
+				# print "fullSet, iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged)
+			iter += 1
+		else:
+			# go over non-bound values
+			nonBoundIs = np.nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+			for i in nonBoundIs:
+				alphaPairsChanged += innerL(i, oS)
+				# print "non-bound, iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged)
+			iter += 1
+		# toggle between entire set and non-bound loop
+		if entireSet: 
+			entireSet = False
+		elif (alphaPairsChanged == 0):
+			entireSet = True
+		print "iteration number: %d" % iter
+	return oS.b, oS.alphas
+
+def calcWs(alphas, dataArr, classLabels):
+	X = np.mat(dataArr)
+	labelMat = np.mat(classLabels).transpose()
+	m, n = np.shape(X)
+	w = np.zeros((n, 1))
+	for i in range(m):
+		# w.transpose = 
+		# sum over i
+		# 	alpha(i) * y(i) * X(i).transpose
+		w += np.multiply(alphas[i] * labelMat[i], X[i, :].T)
+	return w
+
+def testRbf(k1 = 1.3):
+	dataArr, labelArr = loadDataSet('testSetRBF.txt')
+	b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1))
+	dataMat = np.mat(dataArr)
+	labelMat = np.mat(labelArr).transpose()
+	svInd = np.nonzero(alphas.A > 0) [0]
+	# Create matrix of support vectors
+	sVs = dataMat[svInd]
+	labelSV = labelMat[svInd]
+	print "there are %d Support Vectors" % np.shape(sVs)[0]
+	print "b = " + str(b)
+	# for idx in svInd:
+	# 	y = labelMat[idx]
+	# 	print "idx = %d, y = %f" % (idx, y)
+	m, n = np.shape(dataMat)
+	errorCount = 0
+	for i in range(m):
+		kernelEval = kernelTrans(sVs, dataMat[i, :], ('rbf', k1))
+		predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+		if np.sign(predict) != np.sign(labelArr[i]):
+			errorCount += 1
+	print "the training error rate is: %f" % (float(errorCount)/m)
+	w1 = calcWs(alphas, dataArr, labelArr)
+	dataArr, labelArr = loadDataSet('testSetRBF2.txt')
+	errorCount = 0
+	dataMat = np.mat(dataArr)
+	labelMat = np.mat(labelArr).transpose()
+	m, n = np.shape(dataMat)
+	for i in range(m):
+		kernelEval = kernelTrans(sVs, dataMat[i, :], ('rbf', k1))
+		predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+		if np.sign(predict) != np.sign(labelArr[i]):
+			# print "ERROR predict = %f, labelArr[%d] = %f" % (predict, i, labelArr[i])
+			errorCount += 1
+		# else:
+			# print "predict = %f, labelArr[%d] = %f" % (predict, i, labelArr[i])
+	print "the test error rate is: %f" % (float(errorCount)/m)
+	print "error count = %f, m = %f" % (float(errorCount), float(m))
+	return b, alphas, svInd, sVs, w1
+
+
+
+## ----- No kernel versions
+def calcEkK(oS, k):
+	""" calculate an E value for a given alpha. 
+		Before done inline, but in full Platt SMO, 
+		calculation is done more often, so pull it out 
+	"""
+	"""
+		fXi = Prediction of class
+		np.multiply(alphas, labelMat) does element-wise 
+		multiplication resulting in alpha(i) * y(i) terms and 
+		a (1 x m) matrix (after the transform)
+		(dataMatrix * dataMatrix[i,:].T) is the inner product 
+		of all the rows of x with the ith row. 
+		It is a (m x 1) matrix
+		Multiplying these 2, gives the 
+		sum(i=1 to m) [ alpha(i) y(i) <x(i), x>], 
+		the main term in calculating the predicted class.
+	"""
+	fXk = float( np.multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k, :].T) + oS.b)
+	Ek = fXk - float(oS.labelMat[k])
+	return Ek
+
+def innerLK(i, oS):
 	""" original version - other version allows use of kernel
 	"""
 	Ei = calcEk(oS, i)
 	if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or \
-		((oS.labelMat[i]*Ei < oS.tol) and (oS.alphas[i] > oS.C)):
+		((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
 		j, Ej = selectJ(i, oS, Ei)
 		alphaIold = oS.alphas[i].copy()
 		alphaJold = oS.alphas[j].copy()
@@ -257,130 +418,45 @@ def innerLnoKernel(i, oS):
 	else:
 		return 0
 
-def innerL(i, oS):
-	Ei = calcEk(oS, i)
-	if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or \
-		((oS.labelMat[i]*Ei < oS.tol) and (oS.alphas[i] > oS.C)):
-		j, Ej = selectJ(i, oS, Ei)
-		alphaIold = oS.alphas[i].copy()
-		alphaJold = oS.alphas[j].copy()
-		if (oS.labelMat[i] != oS.labelMat[j]):
-			L = max(0, oS.alphas[j] - oS.alphas[i])
-			H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
-		else:
-			L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
-			H = min(oS.C, oS.alphas[j] + oS.alphas[i])
-		if L == H:
-			print "L == H"
-			return 0
-		# Change to use kernel
-		# eta = 2.0 * oS.X[i, :] * oS.X[j, :].T \
-		# 		- oS.X[i, :] * oS.X[i, :].T \
-		# 		- oS.X[j, :] * oS.X[j, :].T
-		eta = 2.0 * oS.K[i, j] - oS.K[i, i] - os.K[j, j]
-		if eta >= 0:
-			print "eta >= 0"
-			return 0
-		oS.alphas[j] -= oS.labelMat[j] * (Ei - Ej) / eta
-		oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
-		# update ecache (j)
-		updateEk(oS, j)
-		if (abs(oS.alphas[j] - alphaJold) < 0.00001):
-			print "j not moving enough"
-			return 0
-		oS.alphas[i] += oS.labelMat[j] * oS.labelMat[i] \
-			* (alphaJold - oS.alphas[j])
-		# update ecache (i)
-		updateEk(oS, i)			
-		if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]):
-			oS.b = b1
-		elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]):
-			oS.b = b2
-		else:
-			oS.b = (b1 + b2)/2.0
-		return 1
-	else:
-		return 0
 
-def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
-	"""  Full Platt SMO - same inputs as simple version
-	"""
-	# create data structure to hold all the data
-	oS = optStruct(np.mat(dataMatIn), np.mat(classLabels).transpose(), C, toler)
-	# initialize variables
-	iter = 0
-	entireSet = True
-	alphaPairsChanged = 0
-	# main code - similar to smoSimple, but more exit conditions
-	while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
-		alphaPairsChanged = 0
-		if entireSet:
-			# go over all values
-			for i in range(oS.m):
-				# call innerL to choose 2nd alpha and do optimization if possible
-				alphaPairsChanged += innerL(i, oS)
-			print "fullSet, iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged)
-			iter += 1
-		else:
-			# go over non-bound values
-			nonBoundIs = np.nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
-			for i in nonBoundIs:
-				alphaPairsChanged += innerL(i, oS)
-				print "non-bound, iter: %d i:%d, pairs changed %d" % (iter, i, alphaPairsChanged)
-			iter += 1
-		# toggle between entire set and non-bound loop
-		if entireSet: 
-			entireSet = False
-		elif (alphaPairsChanged == 0):
-			entireSet = True
-		print "iteration number: %d" % iter
-	return oS.b, oS.alphas
-				
+## ---- start testing with already MLIA code instead of mine
 
-def calcWs(alphas, dataArr, classLabels):
-	X = np.mat(dataArr)
-	labelMat = np.mat(classLabels).transpose()
-	m, n = np.shape(X)
-	w = np.zeros((n, 1))
-	for i in range(m):
-		# w.transpose = 
-		# sum over i
-		# 	alpha(i) * y(i) * X(i).transpose
-		w += np.multiply(alphas[i] * labelMat[i], X[i, :].T)
-	return w
-
-def kernelTrans(X, A, kTup):
-""" kTup is a tuple that containes information about the kernel
-	kTup[0]: string describing what type of kernel
-	kTup[1...]: optional argument that may be needed
-"""
-	m, n = np.shape(X)
-	K = np.mat(np.zeros((m, 1)))
-	if kTup[0] == 'lin':
-		# linear kernel
-		# dot product between dataset and row of dataset
-		K = X * A.T
-	elif kTup[0] == 'rbf':
-		# radial basis function (Gaussian)
-		for j in range(m):
-			deltaRow = X[j, :] - A
-			K[j] = deltaRow * deltaRow.T
-		# element-wise division
-		K = exp(K / (-1 * kTup[1] ** 2))
-	# add additional kernel types here
-	# elif ... 
-	else:
-		raise NameError('Houston, we have a problem - that kernel is not recognized')
-	return K
-
-
-
-
-
-
-
-
-
+def testRbfMlia(k1=1.3):
+    dataArr, labelArr = loadDataSet('testSetRBF.txt')
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 10000, ('rbf', k1)) #C=200 important
+    datMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    svInd = np.nonzero(alphas.A > 0)[0]
+    sVs = datMat[svInd] #get matrix of only support vectors
+    labelSV = labelMat[svInd]
+    print "there are %d Support Vectors" % np.shape(sVs)[0]
+    print "b = " + str(b)
+    # for idx in svInd:
+    #     y = labelMat[idx]
+    #     print "idx = %d, y = %f" % (idx, y)
+    m, n = np.shape(datMat)
+    errorCount = 0
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, datMat[i, :], ('rbf', k1))
+        predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+        if np.sign(predict) != np.sign(labelArr[i]): errorCount += 1
+    print "the training error rate is: %f" % (float(errorCount)/m)
+    w1 = calcWs(alphas, dataArr, labelArr)
+    dataArr, labelArr = loadDataSet('testSetRBF2.txt')
+    errorCount = 0
+    datMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    m, n = np.shape(datMat)
+    for i in range(m):
+        kernelEval = kernelTrans(sVs, datMat[i, :], ('rbf', k1))
+        predict = kernelEval.T * np.multiply(labelSV, alphas[svInd]) + b
+        if np.sign(predict)!= np.sign(labelArr[i]): 
+            # print "ERROR predict = %f, labelArr[%d] = %f" % (predict, i, labelArr[i])
+            errorCount += 1    
+        # else:
+            # print "predict = %f, labelArr[%d] = %f" % (predict, i, labelArr[i])
+    print "the test error rate is: %f" % (float(errorCount)/m)    
+    return b, alphas, svInd, sVs, w1
 
 
 
